be8141c67963f2ae049d7f31d0c5bb31aa49adda
danilwhale <onefiler1@hotmail.com>
2023-03-15 16:05:32 +0200

    long time fixes and tile pack system adapted all ui elements create tile pack manager added fps counter tried to optimize tiles

---
+++ TilePacks/TilePackManager.cs
@@ -0,0 +1,45 @@
+using BuildingGame.Tiles;
+
+namespace BuildingGame.TilePacks;
+
+public static class TilePackManager
+{
+    public static List<TilePack> TilePacks { get; } = new List<TilePack>();
+    public static event Action? PackChanged;
+
+    public static void LoadPacks(string root = "packs")
+    {
+        TilePacks.Clear();
+        
+        if (!Directory.Exists(root))
+        {
+            Directory.CreateDirectory(root);
+            return;
+        }
+
+        foreach (var packFolder in Directory.GetDirectories(root).Where(s => TilePack.Check(s)))
+        {
+            TilePacks.Add(new TilePack(packFolder));
+        }
+    }
+
+    public static void SetDefaultPack()
+    {
+        Program.atlas = Program.origAtlas;
+        Tile.DefaultTiles = Tile.GenerateTiles();
+        PackChanged?.Invoke();
+    }
+
+    public static void ApplyPack(string name)
+    {
+        var pack = TilePacks.Find(m => m.Name == name);
+        if (pack.Equals(default(TilePack))) return;
+        pack.Apply();
+        PackChanged?.Invoke();
+    }
+
+    internal static void UnloadPacks()
+    {
+        foreach (var pack in TilePacks) pack.Unload();
+    }
+}
