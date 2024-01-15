bdd615ef4fc1960238f52f9aad1b9693af0f6a0f
danil <onefiler1@hotmail.com>
2024-01-15 10:53:17 +0200

    make tooltip work like in original

--- UI/Elements/Tooltip.cs
+++ UI/Elements/Tooltip.cs
@@ -1,13 +1,18 @@
 using System.Numerics;
+using BuildingGame.UI.Brushes;
 
 namespace BuildingGame.UI.Elements;
 
-public class Tooltip : TextElement
+public class Tooltip : Element
 {
+    public string Text;
+    public float TextSize;
+    private GradientBrush _brush;
+    
     public Tooltip(ElementId id) : base(id)
     {
+        _brush = new GradientBrush(new Color(0, 0, 0, 50), new Color(16, 0, 16, 127));
         Visible = false;
-        TextAlignment = Alignment.CenterLeft;
     }
 
     public override void Update()
@@ -23,6 +28,16 @@ public class Tooltip : TextElement
         }
         else Visible = false;
 
-        Position = GetMousePosition() + new Vector2(16);
+        var pos = GetMousePosition() + new Vector2(0, 16);
+        var size = MeasureTextEx(GetFontDefault(), Text, TextSize, GetSpacing(TextSize));
+
+        Position = pos with { X = pos.X - 16.0f };
+        Size = size + new Vector2(16);
+    }
+
+    protected override void Render()
+    {
+        _brush.FillArea(new Rectangle(0, 0, Size.X, Size.Y));
+        DrawText(Text, 8.0f, 8.0f, TextSize, Color.WHITE);
     }
 }
